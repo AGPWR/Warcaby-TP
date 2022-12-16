6059f9345b2a1c8c7a5e41bb30b138f110a8d464
wojtek <268486@student.pwr.edu.pl>
2022-12-16 21:36:09 +0100

    Adding basic implementation of board

--- src/main/java/com/example/Game.java
+++ src/main/java/com/example/Game.java
@@ -1,7 +1,22 @@
 package com.example;
 
-public class Game {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Gboard gboard=new Gboard(8);
+    Scene scene=new Scene(gboard.gridPane);
+    stage.setScene(scene);
+    stage.setTitle("");
+    stage.setResizable(false);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
 }
