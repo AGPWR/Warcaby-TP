0a3371229eb86703fb82f27ccc1c14b0169919f3
wojtek <268486@student.pwr.edu.pl>
2022-12-17 22:16:39 +0100

    some simple modifications

--- src/main/java/com/example/Board.java
+++ src/main/java/com/example/Board.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class Board {
-  private int[][][] positions;
+  private int[][] positions;
   
   int getColor(int x,int y){
     if((x+y)%2!=0){
@@ -11,48 +11,8 @@ public class Board {
       return 0;
   }
 
-  Boolean isPone(int x, int y){
-    if (this.positions[x][y][0]==1){
-      return true;
-    }
-    else
-      return false;
-  }
-  void addPone(int x, int y){
-    this.positions[x][y][0]=1;
-  }
-  void removePone(int x, int y){
-    this.positions[x][y][0]=0;
-  }
-  void movePone(int x1, int y1,int x2,int y2 ){
-    removePone(x1,y1);
-    addPone(x2,y2);
-  }
-
-  //Trzeba dodać wyjatek sprawdzjacy czy n jest parzyste
   Board(int n){
-    this.positions=new int[n][n][1];
-    for(int i=0;i<=(n-2)/2;i++){
-      for(int j=0;j<n;j++){
-          if(getColor(i,j)==1){
-            this.positions[i][j][0]=1;
-          }
-          else{
-            this.positions[i][j][0]=0;
-          }
-      }
-    }
-    for(int i=0;i<=(n-2)/2+3;i++){
-      for(int j=0;j<n;j++){
-        if(getColor(i,j)==1){
-          this.positions[i][j][0]=1;
-        }
-        else{
-          this.positions[i][j][0]=0;
-        }
-      }
-    }
-
+    this.positions=new int[n][n];
   }
 
 
--- src/main/java/com/example/Gboard.java
+++ src/main/java/com/example/Gboard.java
@@ -1,28 +1,30 @@
 package com.example;
+
 import javafx.scene.layout.GridPane;
 
 public class Gboard {
 
   private Board board;
-  private int rowsNumber;
+  private final int rowsNumber;
   GridPane gridPane;
 
-  private void insert(){
-      for(int i=0;i<rowsNumber;i++){
-        for(int j=0;j<rowsNumber;j++){
-          Square square =new Square();
-          square.setColor(board.getColor(i,j));
-          gridPane.add(square.square,i,j,1,1);
-        }
+  private void insert() {
+    for (int i = 0; i < rowsNumber; i++) {
+      for (int j = 0; j < rowsNumber; j++) {
+        Square square = new Square();
+        square.setColor(board.getColor(i, j));
+        gridPane.add(square.square, j, i, 1, 1);
       }
+    }
   }
 
 
-  Gboard(int n){
-    this.gridPane=new GridPane();
-    this.rowsNumber=n;
-    this.board=new Board(n);
+  Gboard(int n) {
+    this.gridPane = new GridPane();
+    this.rowsNumber = n;
+    this.board = new Board(n);
     insert();
+
   }
 
 
