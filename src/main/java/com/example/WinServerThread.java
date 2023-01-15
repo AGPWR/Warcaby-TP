8ea9cc06d56562adc10d888154a68753a5f6e04a
wojtek <268486@student.pwr.edu.pl>
2023-01-15 17:03:00 +0100

    Fixing server bug

--- src/main/java/com/example/WinServerThread.java
+++ src/main/java/com/example/WinServerThread.java
@@ -13,29 +13,31 @@ public class WinServerThread {
    * @param args
    */
   public static void main(String[] args) {
+    while (true) {
+      try (ServerSocket serverSocket = new ServerSocket(4444)) {
 
-    try (ServerSocket serverSocket = new ServerSocket(4444)) {
+        System.out.println("Server is listening on port 4444");
 
-      System.out.println("Server is listening on port 4444");
+        while (true) {
+          Socket firstClient = serverSocket.accept();
+          System.out.println("First client connected");
+          System.out.println("Waiting for the second player");
 
-      while (true) {
-        Socket firstClient = serverSocket.accept();
-        System.out.println("First client connected");
-        System.out.println("Waiting for the second player");
+          Socket secondClient = serverSocket.accept();
+          System.out.println("Second client connected");
 
-        Socket secondClient = serverSocket.accept();
-        System.out.println("Second client connected");
+          Game g = new Game(firstClient, secondClient);
+          Thread gTh = new Thread(g);
+          gTh.start();
 
-        Game g = new Game(firstClient, secondClient);
-        Thread gTh = new Thread(g);
-        gTh.start();
 
+        }
+
+      } catch (IOException ex) {
+        System.out.println("Server exception: " + ex.getMessage());
+        ex.printStackTrace();
 
       }
-
-    } catch (IOException ex) {
-      System.out.println("Server exception: " + ex.getMessage());
-      ex.printStackTrace();
     }
   }
 }
